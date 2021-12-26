1067a5ad4bd3d109eb0f18fdab12c6c578fc2529
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2021-12-26 13:33:24 +0100

    Add mail_data with example data

---
+++ src/mail_data.py
@@ -0,0 +1,5 @@
+"""Store secret data as the email addresses orthe resolution domain to land."""
+
+# This secret.py only contains example data. Replace the values with your own.
+EMAIL_ADDRESSES_STRING = """dummy_mail@pm.me <dummy_mail@pm.me>; dummy_mail@googlemail.com <dummy_mail@googlemail.com>;foo@bar.de <foo@bar.de>"""
+GENERIC_DOMAINS_TO_COUNTRY = {"bar.de": "Germany"}
